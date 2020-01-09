318b4bc3185597b47a2b9a1d0513ad4bbd11437e
alswns <49472611+alswns@users.noreply.github.com>
2020-01-10 04:53:19 +0900

    Updates

---
+++ precache-manifest.bcdec546fc9f2c19d702d32574468a74.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2612c93832f3243f8dc3eacd68183e07",
+    "url": "/alswns.github.io/index.html"
+  },
+  {
+    "revision": "dd24ccb0053185facded",
+    "url": "/alswns.github.io/static/css/main.6c5aab70.chunk.css"
+  },
+  {
+    "revision": "f52f575a8f00c2d5f976",
+    "url": "/alswns.github.io/static/js/2.b4c67c38.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/alswns.github.io/static/js/2.b4c67c38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd24ccb0053185facded",
+    "url": "/alswns.github.io/static/js/main.b0f85741.chunk.js"
+  },
+  {
+    "revision": "f1467e296bd165157f94",
+    "url": "/alswns.github.io/static/js/runtime-main.0b084e85.js"
+  },
+  {
+    "revision": "e68f5a826ff2efa8d305ff8daee9dcc2",
+    "url": "/alswns.github.io/static/media/facebook.e68f5a82.png"
+  },
+  {
+    "revision": "347b368f1598fc12588c8cbff69762e1",
+    "url": "/alswns.github.io/static/media/net.347b368f.png"
+  },
+  {
+    "revision": "acbd532b406d5b8510a306884c783b80",
+    "url": "/alswns.github.io/static/media/self_introduce.acbd532b.png"
+  },
+  {
+    "revision": "42623222ba9fca390fbe9c4111ea4c6a",
+    "url": "/alswns.github.io/static/media/sos.42623222.png"
+  },
+  {
+    "revision": "0dad915bf9e61dc5f0ff50282171a273",
+    "url": "/alswns.github.io/static/media/view_headline-24px.0dad915b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a254a5a9b3d90c5b6d41b1c3fd9a1e6.js"
+  "/alswns.github.io/precache-manifest.bcdec546fc9f2c19d702d32574468a74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alswns.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
