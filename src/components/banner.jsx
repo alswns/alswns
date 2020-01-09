0845e5b8b61b4fd2c687d9790f67e2f2149056a9
alswns <49472611+alswns@users.noreply.github.com>
2020-01-10 00:53:46 +0900

    first

---
+++ src/components/banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../CSS/banner.css'
+function Banner(){
+
+    return(
+        <span className='wapper'>
+        <div className='left'>박민준</div>
+        <div className='center'></div>
+        <div className='right'>
+            <ul>
+                <li>Github</li>
+                <li>Profile</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+
+        </div>
+        </span>
+    )
+      
+    
+
+}
+
+export default Banner
